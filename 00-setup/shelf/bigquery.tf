ca534da6a05a5af0e7d484148624d1feebd0b9bf
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-21 16:27:56 -0500

    Update bigquery.tf

--- 00-setup/shelf/bigquery.tf
+++ 00-setup/shelf/bigquery.tf
@@ -23,9 +23,9 @@ resource "google_bigquery_connection" "bq_external_connection_creation" {
 resource "google_project_iam_member" "bq_connection_gmsa_iam_role_grant" {
     project = var.project_id
     role = "roles/storage.objectViewer"
-    member = format("serviceAccount:%s", google_bigquery_connection.create_bq_external_connection.cloud_resource[0].service_account_id)
+    member = format("serviceAccount:%s", google_bigquery_connection.bq_external_connection_creation.cloud_resource[0].service_account_id)
 
-    depends_on = [google_bigquery_connection.create_bq_external_connection]
+    depends_on = [google_bigquery_connection.bq_external_connection_creation]
 
 }
 
