9d9de2fbbc764a736d57225245546baae1a4f9fc
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-21 17:46:19 -0500

    Update bash.tf

--- 00-setup/shelf/bash.tf
+++ 00-setup/shelf/bash.tf
@@ -5,10 +5,11 @@ Run bash scripts
 resource "null_resource" "run_bash_script" {
     provisioner "local-exec" {
 
-        command = "/bin/bash ../02-scripts/bash/hello_world_bash_sample.sh ${local.admin_upn_fqn} ${local.location} gs://${local.spark_bucket}/dummy "
+        command = "/bin/bash ../02-scripts/bash/run_service_updates.sh ${local.admin_upn_fqn} ${local.location} "
     }
     depends_on = [
         time_sleep.sleep_after_network_and_storage_steps
+        google_dataproc_metastore_service.datalake_metastore_creation
     ]
 }
 
