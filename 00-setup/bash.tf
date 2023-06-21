630eaa2d995dcc165b220eadba885487f879cb56
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-21 18:17:52 -0500

    Update bash.tf

--- 00-setup/bash.tf
+++ 00-setup/bash.tf
@@ -8,7 +8,7 @@ resource "null_resource" "run_bash_script" {
         command = "/bin/bash ../01-scripts/bash/run_service_updates.sh ${local.dpms_nm} ${local.location} "
     }
     depends_on = [
-        time_sleep.sleep_after_network_and_storage_steps
+        time_sleep.sleep_after_network_and_storage_steps,
         google_dataproc_metastore_service.datalake_metastore_creation
     ]
 }
