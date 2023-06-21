0c32c570b3b8018c46f9ce9cb99b41a19d7a7ff5
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-21 17:26:39 -0500

    Update bigquery.tf

--- 00-setup/bigquery.tf
+++ 00-setup/bigquery.tf
@@ -1,16 +1,19 @@
 resource "google_bigquery_dataset" "bq_raw_dataset_creation" {
   dataset_id                  = local.bq_raw_dataset
   location                    = local.location
+  depends_on = [time_sleep.sleep_after_identities_permissions]
 }
 
 resource "google_bigquery_dataset" "bq_curated_dataset_creation" {
   dataset_id                  = local.bq_curated_dataset
   location                    = local.location
+  depends_on = [time_sleep.sleep_after_identities_permissions]
 }
 
 resource "google_bigquery_dataset" "bq_product_dataset_creation" {
   dataset_id                  = local.bq_product_dataset
   location                    = local.location
+  depends_on = [time_sleep.sleep_after_identities_permissions]
 }
 
 resource "google_bigquery_connection" "bq_external_connection_creation" {
@@ -18,6 +21,7 @@ resource "google_bigquery_connection" "bq_external_connection_creation" {
     project = var.project_id
     location = local.location
     cloud_resource {}
+    depends_on = [time_sleep.sleep_after_identities_permissions]
 } 
 
 resource "google_project_iam_member" "bq_connection_gmsa_iam_role_grant" {
