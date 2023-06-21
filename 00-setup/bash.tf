8233711fe7e843b9be139ebcbb90b2a765c17a7d
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-21 18:07:22 -0500

    Update bash.tf

--- 00-setup/bash.tf
+++ 00-setup/bash.tf
@@ -5,7 +5,7 @@ Run bash scripts
 resource "null_resource" "run_bash_script" {
     provisioner "local-exec" {
 
-        command = "/bin/bash ../02-scripts/bash/run_service_updates.sh ${local.admin_upn_fqn} ${local.location} "
+        command = "/bin/bash ../01-scripts/bash/run_service_updates.sh ${local.dpms_nm} ${local.location} "
     }
     depends_on = [
         time_sleep.sleep_after_network_and_storage_steps
