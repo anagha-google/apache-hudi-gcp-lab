5f215b4fff9b179fbcab2e0443bcdd07aefc02cd
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-21 22:23:13 -0500

    Update dataplex.tf

--- 00-setup/dataplex.tf
+++ 00-setup/dataplex.tf
@@ -1,1 +1,53 @@
+resource "google_dataplex_lake" "create_lake" {
+ for_each = {
+    "${local.lake_nm}/Data Lake" : "owner=${local.resource_prefix}",
+  }
+  location     = local.location
+  name         = element(split("/", each.key), 0)
+  description  = element(split("/", each.key), 1)
+  display_name = element(split("/", each.key), 1)
 
+  labels       = {
+    element(split("=", each.value), 0) = element(split("=", each.value), 1)
+  }
+  
+  project = local.project_id
+}
+
+resource "time_sleep" "sleep_after_lake_creation" {
+  create_duration = "120s"
+  depends_on = [google_dataplex_lake.create_lakes]
+}
+
+
+resource "google_dataplex_zone" "create_zones" {
+ for_each = {
+    "${local.resource_prefix}-raw-zone/Raw Zone/RAW" : ""
+    "${local.resource_prefix}-curated-zone/Curated Zone/CURATED" : ""
+    "${local.resource_prefix}-product-zone/Product Zone/CURATED" : ""
+  }
+
+  project      = var.project_id
+  lake         =  local.lake_nm
+  location     =  local.location
+  name         = element(split("/", each.key), 0)
+  description  = element(split("/", each.key), 1)
+  display_name = element(split("/", each.key), 1)
+  type         = element(split("/", each.key), 2)
+
+  discovery_spec {
+    enabled = true
+    schedule = "0 * * * *"
+  }
+
+  resource_spec {
+    location_type = "SINGLE_REGION"
+  }
+
+  depends_on  = [time_sleep.sleep_after_lake_creation]
+}
+
+resource "time_sleep" "sleep_after_dataplex_deployments" {
+  create_duration = "120s"
+  depends_on = [google_dataplex_lake.create_zones]
+}
