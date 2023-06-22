54af5d3285e9436ac5ac467a465beebe882c4780
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-22 15:56:00 -0500

    Update create_taxi_trips_table.sql

--- 01-scripts/bqsql/create_taxi_trips_table.sql
+++ 01-scripts/bqsql/create_taxi_trips_table.sql
@@ -1,4 +1,3 @@
-create or replace table `gaia_nyc_taxi_trips_ds.taxi_trips` partition by partition_date as 
 SELECT
 'yellow' as taxi_type,
 extract(year from pickup_datetime) as trip_year,
@@ -598,3 +597,36 @@ date(pickup_datetime) as partition_date,
 cast(null as numeric) as distance_between_service,
 cast(null as integer) as time_between_service
 FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2013`
+UNION ALL
+SELECT
+'yellow' as taxi_type,
+extract(year from pickup_datetime) as trip_year,
+extract(month from pickup_datetime) as trip_month,
+extract(day from pickup_datetime) as trip_day,
+extract(hour from pickup_datetime) as trip_hour,
+extract(minute from pickup_datetime) as trip_minute,
+vendor_id as vendor_id,
+pickup_datetime as pickup_datetime,
+dropoff_datetime as dropoff_datetime,
+store_and_fwd_flag as store_and_forward,
+Rate_Code as rate_code,
+pickup_location_id as pickup_location_id,
+dropoff_location_id as dropoff_location_id,
+Passenger_Count as passenger_count,
+trip_distance,
+fare_amount,
+imp_surcharge as surcharge,
+mta_tax as MTA_tax,
+tip_amount,
+tolls_amount,
+cast(null as numeric) as improvement_surcharge,
+total_amount,
+payment_type as payment_type_code,
+cast(null as numeric) as congestion_surcharge,
+cast(null as string) as trip_type,   
+cast(null as numeric) as ehail_fee,   
+date(pickup_datetime) as partition_date,
+cast(null as numeric) as distance_between_service,
+cast(null as integer) as time_between_service
+FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2012`
+
