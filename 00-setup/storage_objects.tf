95e7970bc09cbd3a74822eb23afec2c8caa729fc
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-06-27 12:09:57 -0500

    Update storage_objects.tf

--- 00-setup/storage_objects.tf
+++ 00-setup/storage_objects.tf
@@ -1,35 +1,37 @@
+variable "scripts_to_upload" {
+  type = map(string)
+  default = {
+    "../01-scripts/airflow/data_engineering_pipeline.py" = "airflow/data_engineering_pipeline.py",
+    "../01-scripts/bash/run_service_updates.sh" = "bash/run_service_updates.sh",
+    "../01-scripts/bqsql/export_taxi_trips.sql" = "bqsql/export_taxi_trips.sql",
+    "../01-scripts/pyspark/nyc_taxi_data_generator_parquet.py" = "pyspark/nyc_taxi_data_generator_parquet.py"
+  }
+}
+
+resource "google_storage_bucket_object" "upload_scripts_to_gcs" {
+  for_each = var.scripts_to_upload
+  name     = each.value
+  source   = "${path.module}/${each.key}"
+  bucket   = "${local.code_bucket}"
+  depends_on = [
+    time_sleep.sleep_after_bucket_creation
+  ]
+}
+
 variable "notebooks_to_upload" {
   type = map(string)
   default = {
-    "../02-notebooks/chicago-crimes-analysis/chicago-crimes-analytics.ipynb" = "chicago-crimes-analysis/chicago-crimes-analytics.ipynb",
-   
+    "../02-notebooks/nyc_taxi_trips_data_generator/nyc_taxi_hudi_data_generator.ipynb" = "nyc_taxi_trips_data_generator/nyc_taxi_hudi_data_generator.ipynb"
   }
 }
 resource "google_storage_bucket_object" "upload_notebooks_to_gcs" {
   for_each = var.notebooks_to_upload
   name     = each.value
   source   = "${path.module}/${each.key}"
-  bucket   = "${local.lab_notebook_bucket}"
+  bucket   = "${local.notebook_bucket}"
   depends_on = [
     time_sleep.sleep_after_bucket_creation
   ]
-
 }
 
-variable "scripts_to_upload" {
-  type = map(string)
-  default = {
-    "../notebooks/chicago-crimes-analysis/chicago-crimes-analytics.ipynb" = "chicago-crimes-analysis/chicago-crimes-analytics.ipynb",
-   
-  }
-}
-resource "google_storage_bucket_object" "upload_scripts_to_gcs" {
-  for_each = var.scripts_to_upload
-  name     = each.value
-  source   = "${path.module}/${each.key}"
-  bucket   = "${local.lab_notebook_bucket}"
-  depends_on = [
-    time_sleep.sleep_after_bucket_creation
-  ]
 
-}
